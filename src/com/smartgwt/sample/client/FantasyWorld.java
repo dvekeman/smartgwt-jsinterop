bfd84298165ae623c0ca6cc4c43a213ba61ba342
dvekeman <dieter.vekeman@gmail.com>
2019-01-18 17:14:39 +0100

    Solution

--- src/com/smartgwt/sample/client/FantasyWorld.java
+++ src/com/smartgwt/sample/client/FantasyWorld.java
@@ -3,14 +3,21 @@ package com.smartgwt.sample.client;
 import java.util.HashSet;
 import java.util.Set;
 
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+@JsType(namespace = "fantasy")
 public class FantasyWorld {
 
+	@JsProperty(name = "name")
 	private String name = "Planet";
 
+	@JsProperty(name = "cities")
 	private Set<String> cities = new HashSet<>();
 
 	public FantasyWorld() {
 	}
+
 
 	public String changeName(String name) {
 		this.name = name;
@@ -28,4 +35,18 @@ public class FantasyWorld {
 	// -------------------------------------------------------------------------------------------------------------- //
 	// Remove all below this line
 	// -------------------------------------------------------------------------------------------------------------- //
+	public String addAllCities() {
+		String[] allCities = new String[]{
+				"Midgar", "Kalm", "Fort Condor", "Junon",
+				"Costa del Sol", "North Corel", "Gongaga",
+				"Cosmo Canyon"
+		};
+		for (String city : allCities) {
+			// This is the native method
+			addCity(city);
+		}
+		return showCities();
+	}
+
+	public native void addCity(String city);
 }
--- war/js/fantasy.js
+++ war/js/fantasy.js
@@ -1,8 +1,43 @@
 // --------------------------------------------------------------------------------------------------------- //
 // Remove all between these lines ->
 // --------------------------------------------------------------------------------------------------------- //
+// Add a custom label once the GWT module is initialized
+// Add a new function `addCity` to an existing object (type) `FantasyWorld`
+function initializeWorld() {
+  // `prototype` => modify the FantasyWorld type
+  fantasy.FantasyWorld.prototype.addCity = function (cityName) {
+    this.cities.add(cityName);
+  };
+}
 
-// --------------------------------------------------------------------------------------------------------- //
+// Create the cities DataSource and bind to the data in cities.data.js
+var citiesDs = isc.DataSource.create({
+  ID: "citiesDS",
+  dataFormat: "json",
+  dataURL: "js/cities.data.js",
+  fields:
+    [ { name: "cityName", title: "Name", type: "text" } ]
+});
+
+var citiesRecord = {
+  dsName: "citiesDS",
+  dsTitle: "Cities"
+};
+
+// Doesn't work :-( (don't know why)
+function addCitiesToGrid1() {
+  var mainGrid = j2js.Registry.lookup("MainListGrid");
+  mainGrid.addData(citiesRecord);
+}
+
+// Our new function
+function addCitiesToGrid() {
+  // Lookup in the registry
+  var mainModule = j2js.Registry.lookup("BuiltInDS");
+
+  // Add our custom JS `ListGridRecord`
+  mainModule.addJSData(citiesRecord);
+}// --------------------------------------------------------------------------------------------------------- //
 // <- Remove all between these lines
 // --------------------------------------------------------------------------------------------------------- //
 
